203164d405b7308e4aca9a9dc65958798084bfd4
SaurabhTalekar <talekarsaurabh1@gmail.com>
2025-06-15 20:44:36 +0530

    by saurabh

--- src/test/java/pkg1/ContactTest.java
+++ src/test/java/pkg1/ContactTest.java
@@ -8,7 +8,7 @@ public class ContactTest {
 	@Test
 	public void Createcontact() {
 		System.out.println("Execute CreatecontactTest");
-		System.out.println("this is new added from sel_pj");
+		System.out.println("this is new1 added from sel_pj");
 		
 		
 
